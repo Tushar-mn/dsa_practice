a48ed828c49f4c55bf61801ac7d2506ae31716d6
Tushar Munakhiya <85923334+Tushar-mn@users.noreply.github.com>
2025-06-20 03:10:07 +0530

    Time: 18 ms (8.26%) | Memory: 24 MB (6.52%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for(int val: nums){
+            freq[val]++;
+        }
+
+        for(auto val: nums){
+            if(freq[val] == 1){
+                return val;
+                break;
+            }
+        }
+        return -1;
+    }
+};
