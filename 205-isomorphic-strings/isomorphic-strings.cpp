fa9c6296e775f2acaebe71fb7258ff116e835769
Tushar Munakhiya <85923334+Tushar-mn@users.noreply.github.com>
2025-06-26 00:27:28 +0530

    Time: 3 ms (42.82%) | Memory: 9.5 MB (28.98%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+
+        unordered_map<char, char> ct;
+        unordered_map<char, char> tc;
+
+        for(int i=0; i<s.size(); i++){
+            int c = s[i];
+            int c2 = t[i];
+
+            if(ct.count(c)){
+                if(ct[c] != c2){
+                    return false;
+                }
+            } else {
+                ct[c] = c2;
+            }
+
+            if(tc.count(c2)){
+                if(tc[c2] != c){
+                    return false;
+                }
+            } else {
+                tc[c2] = c;
+            }
+        }
+
+        return true;
+    }
+};
