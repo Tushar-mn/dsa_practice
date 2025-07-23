00399b787e794b72e09eb0191b2dd18aa9fd69b5
Tushar Munakhiya <85923334+Tushar-mn@users.noreply.github.com>
2025-07-24 02:46:15 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (75.33%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> freq;
-        vector<int> inter;
 
+    bool BinarySearch(vector<int>& nums1, int target){
+        int lo = 0;
+        int hi = nums1.size()-1;
+
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+
+            if(nums1[mid] == target){
+                nums1.erase(nums1.begin()+mid); // removing occurence
+                return true;
+            } else if(nums1[mid] < target){
+                lo = mid+1;
+            } else {
+                hi = mid-1;
+            }
+        }
+
+        return false;
+    }
+
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         if(nums1.size() > nums2.size()){
             swap(nums1, nums2);
         }
 
-        for(int val: nums1){
-            freq[val]++;
-        }
+        sort(nums2.begin(), nums2.end());
+        vector<int> inter;
 
-        for(int val: nums2){
-            if(freq[val] > 0){
-                inter.push_back(val);
-                freq[val]--;
+        for(int num: nums1){
+            if(BinarySearch(nums2, num)){
+                inter.push_back(num);
             }
         }
 
