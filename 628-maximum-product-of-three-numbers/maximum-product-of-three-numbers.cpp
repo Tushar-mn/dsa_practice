39a83b2e2883004f83ccfd8fb9f0abef09721e03
Tushar Munakhiya <85923334+Tushar-mn@users.noreply.github.com>
2025-08-01 02:00:43 +0530

    Time: 7 ms (55.49%) | Memory: 31.4 MB (72.45%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -5,9 +5,6 @@ public:
 
         int n = nums.size();
 
-        int mul1 = nums[n-1]*nums[n-2]*nums[n-3];
-        int mul2 = nums[0]*nums[1]*nums[n-1];
-
-        return max(mul1, mul2);
+        return max(nums[n-1]*nums[n-2]*nums[n-3], nums[0]*nums[1]*nums[n-1]);
     }
 };
