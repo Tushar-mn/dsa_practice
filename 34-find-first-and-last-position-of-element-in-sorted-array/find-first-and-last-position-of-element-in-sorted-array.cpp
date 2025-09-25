f879c1ffac0000233bcfd4474b4921ebec29a593
Tushar Munakhiya <85923334+Tushar-mn@users.noreply.github.com>
2025-09-25 20:59:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (86.39%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,50 @@
 class Solution {
 public:
+
+    int firstOccurence(vector<int>& nums, int target){
+        int lo = 0;
+        int hi = nums.size()-1;
+        int ans = -1;
+
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+
+            if(nums[mid] == target){
+                ans = mid;
+                hi = mid-1;
+            } else if(nums[mid] > target){
+                hi = mid-1;
+            } else {
+                lo = mid+1;
+            }
+        } 
+        return ans;   
+    }
+
+    int secOccurence(vector<int>& nums, int target){
+        int lo = 0;
+        int hi = nums.size()-1;
+        int ans = -1;
+
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+
+            if(nums[mid] == target){
+                ans = mid;
+                lo = mid+1;
+            } else if(nums[mid] > target){
+                hi = mid-1;
+            } else {
+                lo = mid+1;
+            }
+        }
+        return ans;
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        int f = first(nums, target);
-        int l = last(nums, target);
+        int first = firstOccurence(nums, target);
+        int sec = secOccurence(nums, target);
 
-        return{f, l};
-    }
-
-    int first(vector<int>& nums, int target){
-        int lo = 0;
-        int hi = nums.size()-1;
-        int res = -1;
-
-        while(lo <= hi){
-            int mid = lo + (hi-lo)/2;
-
-            if(nums[mid] == target){
-                res = mid;
-                hi = mid-1;
-            } else if(nums[mid] < target){
-                lo = mid+1;
-            } else {
-                hi = mid-1;
-            }
-        }
-        return res;
-    }
-
-    int last(vector<int>& nums, int target){
-        int lo = 0;
-        int hi = nums.size()-1;
-        int res = -1;
-
-        while(lo <= hi){
-            int mid = lo + (hi-lo)/2;
-
-            if(nums[mid] == target){
-                res = mid;
-                lo = mid+1;
-            } else if(nums[mid] < target){
-                lo = mid+1;
-            } else {
-                hi = mid-1;
-            }
-        }
-
-        return res;
+        return {first, sec};
     }
 };
