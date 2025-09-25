f3434cc9fafda76a52822631d968d127554572ed
Tushar Munakhiya <85923334+Tushar-mn@users.noreply.github.com>
2025-09-25 23:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (94.09%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int lo = 0;
+        int hi = s.size()-1;
+
+        while(lo <= hi){
+            while(lo<hi && !isalnum(s[lo])) lo++;
+            while(lo<hi && !isalnum(s[hi])) hi--;
+
+            if(tolower(s[lo]) != tolower(s[hi])){
+                return false;
+            }
+
+            lo++;
+            hi--;
+        }
+
+        return true;
+    }
+};
